da033d9fcc6909802ebd181d22118a4d7559e63c
shaibal1 <shaibal.tiller@gmail.com>
2026-07-07 18:57:32 +0600

    chore: remove old temporary scripts

--- fix.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/App.tsx', 'utf8');
-code = code.replace('<LogOut,\n  Clock className="w-4 h-4" />', '<LogOut className="w-4 h-4" />');
-fs.writeFileSync('src/App.tsx', code);
